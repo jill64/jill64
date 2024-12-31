8605de51d685c329a5ecff1dbe40a916a2d7887b
jill64 <intents.turrets0h@icloud.com>
2024-12-31 12:06:16 +0900

    refactor: update toast usage and improve state management in error handling

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,6 +1,5 @@
 import { init } from '@jill64/sentry-sveltekit-cloudflare/client'
 import { toast } from '@jill64/svelte-suite'
-import { get } from 'svelte/store'
 
 const onError = init(
   'https://b60ad64620e680a1132d402e54add065@o4505814639312896.ingest.us.sentry.io/4506897338662912'
@@ -8,6 +7,6 @@ const onError = init(
 
 export const handleError = onError((e) => {
   if (e instanceof Error) {
-    get(toast).error(e.message)
+    toast.error(e.message)
   }
 })
